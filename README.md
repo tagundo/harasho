9500e6db4a38ba5e114425e539dcf705e52b2615
RusdiNgawi <naufalfajrifix@gmail.com>
2024-04-30 01:30:54 +0700

    remigrate

---
+++ README.md
@@ -0,0 +1,10 @@
+# Harasho
+
+This is the repository that handles versions of the game that [elichika](https://codeberg.org/RusdiNgawi/elichika) can load. This include:
+
+- `masterdata.db` and other client databases.
+- Beatmaps.
+
+This repository is kept up-to-date with `elichika`'s feature updates, with the root being the state of the database at the game's EOS.
+
+Note that server-sided contents like gachas or exchanges is not included here.
---
+++ sql/jp/003.dictionary_ja_k.db.sql
@@ -0,0 +1,34 @@
+-- SIFAS Plus
+-- Restoration stuff
+-- Translated with ChatGPT
+
+INSERT INTO m_dictionary VALUES ('live_appeal_time_mission_got_heal','{0}スタミナを獲得！');
+INSERT INTO m_dictionary VALUES ('live_appeal_time_mission_got_shield','{0}シールドを獲得！');
+INSERT INTO m_dictionary VALUES ('live_appeal_time_mission_got_voltage_by_vo','VOで{0}ボルテージを獲得！');
+INSERT INTO m_dictionary VALUES ('live_appeal_time_mission_got_voltage_by_sp','SPで{0}ボルテージを獲得！');
+INSERT INTO m_dictionary VALUES ('live_appeal_time_mission_got_voltage_by_gd','GDで{0}ボルテージを獲得！');
+INSERT INTO m_dictionary VALUES ('live_appeal_time_mission_got_voltage_by_sk','SKで{0}ボルテージを獲得！');
+INSERT INTO m_dictionary VALUES ('item_desc_1402','超希少なインサイトスキルをリーダーが発見することを保証するトレーニングアイテム。');
+INSERT INTO m_dictionary VALUES ('item_name_1402','ひらめきバッヂ★3');
+INSERT INTO m_dictionary VALUES ('item_thumb_title_1402','★3');
+INSERT INTO m_dictionary VALUES ('item_desc_1501','超希少アイテムのドロップ率を上げるトレーニングアイテム。');
+INSERT INTO m_dictionary VALUES ('item_name_1501','幸運のおまもり★2');
+INSERT INTO m_dictionary VALUES ('item_thumb_title_1501','★2');
+INSERT INTO m_dictionary VALUES ('item_desc_1502','超々希少アイテムのドロップ率を上げるトレーニングアイテム。');
+INSERT INTO m_dictionary VALUES ('item_name_1502','幸運のおまもり★3');
+INSERT INTO m_dictionary VALUES ('item_thumb_title_1502','★3');
+UPDATE m_dictionary SET message = 'スクールアイドルの輝き (R)' WHERE id = 'item_name_1800';
+INSERT INTO m_dictionary VALUES ('item_name_1801', 'スクールアイドルの輝き (SR)');
+INSERT INTO m_dictionary VALUES ('item_name_1802', 'スクールアイドルの輝き (UR)');
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'dic_external_movie_url_1';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'dic_external_movie_url_2';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'dic_external_movie_url_6';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'reference_book_movie_url_16';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_first_awakening_home';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_first_awakening_training_tree';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_gacha_gacha_top';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_gacha_home';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_story_clear_home';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_story_clear_story_chapter_select';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_story_clear_story_main';
+UPDATE m_dictionary SET message = 'https://youtu.be/nIq4z2fnzFU' WHERE id = 'review_request_url_story_clear_story_part_select';
