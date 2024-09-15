0061d82eaeea532bd002962a3630844485724bac
RusdiNgawi <naufalfajrifix@gmail.com>
2024-09-15 18:50:35 +0700

    fix again

--- sql/gl/021.masterdata.db.sql
+++ sql/gl/021.masterdata.db.sql
@@ -1,16 +1,10 @@
 -- ix Aishiteru Banzai! position, why is dev make sense of this
 
 -- reset to other member to avoid error
-UPDATE m_live_member_mapping SET member_master_id = 201 WHERE mapping_id = 10022 AND position = 9; -- umi  to kotori
-UPDATE m_live_member_mapping SET member_master_id = 202 WHERE mapping_id = 10022 AND position = 4; -- kotori to rin
-UPDATE m_live_member_mapping SET member_master_id = 203 WHERE mapping_id = 10022 AND position = 8; -- rin to umi
-UPDATE m_live_member_mapping SET card_position = 201 WHERE mapping_id = 10022 AND position = 9; -- umi  to kotori
-UPDATE m_live_member_mapping SET card_position = 202 WHERE mapping_id = 10022 AND position = 4; -- kotori to rin
-UPDATE m_live_member_mapping SET card_position = 203 WHERE mapping_id = 10022 AND position = 8; -- rin to umi
+UPDATE m_live_member_mapping SET position = 204 WHERE mapping_id = 10022 AND position = 9; -- umi  to kotori
+UPDATE m_live_member_mapping SET position = 208 WHERE mapping_id = 10022 AND position = 4; -- kotori to rin
+UPDATE m_live_member_mapping SET position = 209 WHERE mapping_id = 10022 AND position = 8; -- rin to umi
 
-UPDATE m_live_member_mapping SET member_master_id = 3 WHERE mapping_id = 10022 AND position = 9; -- umi  to kotori
-UPDATE m_live_member_mapping SET member_master_id = 5 WHERE mapping_id = 10022 AND position = 4; -- kotori to rin
-UPDATE m_live_member_mapping SET member_master_id = 4 WHERE mapping_id = 10022 AND position = 8; -- rin to umi
-UPDATE m_live_member_mapping SET card_position = 3 WHERE mapping_id = 10022 AND position = 9; -- umi  to kotori
-UPDATE m_live_member_mapping SET card_position = 5 WHERE mapping_id = 10022 AND position = 4; -- kotori to rin
-UPDATE m_live_member_mapping SET card_position = 4 WHERE mapping_id = 10022 AND position = 8; -- rin to umi
+UPDATE m_live_member_mapping SET position = 4 WHERE mapping_id = 10022 AND position = 204; -- umi  to kotori
+UPDATE m_live_member_mapping SET position = 8 WHERE mapping_id = 10022 AND position = 208; -- kotori to rin
+UPDATE m_live_member_mapping SET position = 9 WHERE mapping_id = 10022 AND position = 209; -- rin to umi
