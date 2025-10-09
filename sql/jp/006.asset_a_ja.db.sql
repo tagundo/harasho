1c732ceefda28682a504fa0e453d9caa8336e7a7
JackieKun <naufalfajrifix@gmail.com>
2025-10-09 21:39:37 +0700

    updated event

--- sql/jp/006.asset_a_ja.db.sql
+++ sql/jp/006.asset_a_ja.db.sql
@@ -1,3 +1,3 @@
 -- package verify
 -- rename prefix number to last order if any files added
-UPDATE m_asset_package SET version = "004af20e4c81b136ae6b4f5a3c62f766c6a20dc1", pack_num = (SELECT COUNT(*) FROM m_asset_package_mapping WHERE package_key="main") WHERE package_key = "main";
+UPDATE m_asset_package SET version = "8f4c572c98dc573db1dbe3a74452bd9c06d28187", pack_num = (SELECT COUNT(*) FROM m_asset_package_mapping WHERE package_key="main") WHERE package_key = "main";
--- sql/jp/007.asset_i_ja.db.sql
+++ sql/jp/007.asset_i_ja.db.sql
@@ -1,3 +1,3 @@
 -- package verify
 -- rename prefix number to last order if any files added
-UPDATE m_asset_package SET version = "1cba05e866750e52911a4cceeddd0a1ffb600c30", pack_num = (SELECT COUNT(*) FROM m_asset_package_mapping WHERE package_key="main") WHERE package_key = "main";
+UPDATE m_asset_package SET version = "66574fd0c9d88be5385ca884320f8f189e531d26", pack_num = (SELECT COUNT(*) FROM m_asset_package_mapping WHERE package_key="main") WHERE package_key = "main";
